19a310521443587a402237f9f8671653f32ecb4f
Dmitry Trifonov <slonegg@gmail.com>
2023-08-08 21:56:43 -0700

    initial version of the OctoAI powered service

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# diffusion-octo
+# OctoAI Powered Stable Diffusion
+
+1. Install [OctoAI Tool and Python SDK](https://docs.octoai.cloud/docs/installation-links)
+1. Build image with:
+    ```bash
+    octoai build
+    ```
+1. Run image with:
+    ```bash
+    docker run -it --rm --gpus all -p 8080:8080 faircompute/diffusion-octo:<tag from previous step> 
+    ```
+1. Launch test request via:
+    ```bash
+    python3 test_request.py | jq -r ".output.image_b64" | base64 --decode > result.jpg
+    ```
+1. Push to docker hub using:
+    ```bash
+    docker image tag hub.docker.com/faircompute/diffusion-octo:<tag from previous step> faircompute/diffusion-octo:latest
+    docker push faircompute/diffusion-octo:latest
+    ```
---
+++ service.py
@@ -0,0 +1,30 @@
+import time
+
+import torch
+from diffusers import DiffusionPipeline
+from octoai.service import Service
+from octoai.types import Image, Text
+
+
+class DiffusionOctoService(Service):
+    """An OctoAI service extends octoai.service.Service."""
+    def __init__(self):
+        self.pipe = None
+
+    def setup(self):
+        """Load the model into memory to make running multiple predictions efficient"""
+        start_time = time.time()
+        print(f'Model loaded in {(time.time() - start_time) * 1000:.2f}ms')
+        DiffusionPipeline.download("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16, revision="fp16")
+        pipe = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16, revision="fp16")
+        self.pipe = pipe.to('cuda')
+
+    def infer(self, prompt: Text) -> Image:
+        """Run a single prediction on the model"""
+        prompt = prompt.text
+        print("Prompt:", prompt)
+        print(f"Running on '{torch.cuda.get_device_name()}'")
+        start_time = time.time()
+        image = self.pipe(prompt, num_inference_steps=30).images[0]
+        print(f"Inference done in {(time.time() - start_time) * 1000:.2f}ms")
+        return Image.from_pil(image)
